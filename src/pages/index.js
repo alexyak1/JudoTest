9aa83ffccca53eb2c8f18a3fcf32014190dc9b7c
ekmclks <aleksandr.iakimchuk@ericsson.com>
2021-11-21 14:39:27 +0100

    React know only className not a class

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div class="question-section">
+    <div className="question-section">
       <h1>Welcome home!</h1>
       <p>Here you will find information about judo which should help you at exam for next belt</p>
     </div>
