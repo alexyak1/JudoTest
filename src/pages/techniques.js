3c3c5393f12cebddf08059d0602af06cc0150b36
alexyak1 <yakimchuk.nstu@gmail.com>
2022-04-04 17:08:15 +0200

    style for all techniques page

--- src/pages/techniques.js
+++ src/pages/techniques.js
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from "react";
 
 export default function Techniques() {
-    const url = "http://localhost:8787/techniques";
+    const url = "https://quiz-judo.herokuapp.com/techniques";
     const [items, setItems] = useState([]);
 
-    useEffect((setError) => {
+    useEffect(() => {
         fetch(url)
             .then(res => res.json())
             .then(
                 (result) => {
                     setItems(result)
-                },
-                (error) => {
-                    setError(error);
                 }
             )
     }, [])
@@ -22,19 +19,17 @@ export default function Techniques() {
         <div className='app'>
             <div>
                 <h2>Here is all techniques:</h2>
-                <ul>
-                    {items.map(item => (
-                        <div>
-                            <li style={{ marginLeft: '1em' }} key={item.id}>
-                                <h5>{item.name} belt: {item.belt}</h5>
-                            </li>
-                            <img
-                                src={"https://lh3.googleusercontent.com/d/" + item.image_url.replaceAll('https://drive.google.com/file/d/', '').replaceAll('/view?usp=sharing', '') + "?authuser=0"}
-                                alt="technique"
-                            />
-                        </div>
-                    ))}
-                </ul>
+                {items.map(item => (
+                    <div>
+                        <p key={item.id}>
+                            <h5>{item.name} belt: {item.belt}</h5>
+                        </p>
+                        <img
+                            src={"https://lh3.googleusercontent.com/d/" + item.image_url.replaceAll('https://drive.google.com/file/d/', '').replaceAll('/view?usp=sharing', '') + "?authuser=0"}
+                            alt="technique"
+                        />
+                    </div>
+                ))}
             </div>
         </div>
     );
