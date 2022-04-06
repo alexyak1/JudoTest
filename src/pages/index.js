f062bb1b142cd202ffa495a5857aa2495232b274
alexyak1 <yakimchuk.nstu@gmail.com>
2022-04-06 13:09:19 +0200

    fix home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="question-section">
-      <h1>Welcome 柔道家!</h1>
-      <p>Juudouka, 柔 juu (mjuk) + 道 dou (väg) + 家 ka (person).</p>
-      <p>In this website you will find information about judo which should help you at exam for next belt</p>
-      <p>Under tab test is located quiz for each belt</p>
-      <p>Under tab Techniques is located all techniques you need to graduate</p>
+    <div className="homePageText">
+      <h1 className="homePageText">Welcome 柔道家</h1>
+      <p className="homePageText">Juudouka, 柔 juu (mjuk) + 道 dou (väg) + 家 ka (person).</p>
+      <p className="homePageText">In this website you will find information about judo which should help you at exam for next belt</p>
+      <p className="homePageText">Under tab test is located quiz for each belt</p>
+      <p className="homePageText">Under tab Techniques is located all techniques you need to graduate</p>
     </div >
   );
 };
