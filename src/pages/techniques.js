b5d45f23849ef604f4ce34af0461629d6be005b4
alexyak1 <yakimchuk.nstu@gmail.com>
2022-04-11 17:52:32 +0200

    filter for techniques

--- src/pages/techniques.js
+++ src/pages/techniques.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from "react";
+import Select from 'react-select';
 
 export default function Techniques() {
     const url = "https://quiz-judo.herokuapp.com/techniques";
     const [items, setItems] = useState([]);
+    const [filterParam, setFilterColor] = useState(['yellow']);
+
+    const options = [
+        { value: 'yellow', label: 'yellow' },
+        { value: 'orange', label: 'orange' },
+        { value: 'green', label: 'green' }
+    ]
+
+    const applyFilter = (e) => {
+        setFilterColor(e.value)
+    }
 
     useEffect(() => {
         fetch(url)
@@ -19,14 +31,23 @@ export default function Techniques() {
         <div className='app'>
             <div>
                 <h2>Here is all techniques:</h2>
-                {items.map(item => (
+                <div><p>Select belt color</p>
+                    <div className="abc">
+                        <Select
+                            defaultValue={options[0]}
+                            onChange={applyFilter}
+                            options={options} />
+                    </div>
+                </div>
+
+                {items.filter(item => item.belt == filterParam).map(filteredItem => (
                     <div>
-                        <p key={item.id}>
-                            <h3>{item.name}</h3>
-                            <p>belt: {item.belt}</p>
+                        <p key={filteredItem.id}>
+                            <h3>{filteredItem.name}</h3>
+                            <p>belt: {filteredItem.belt}</p>
                         </p>
                         <img
-                            src={"https://lh3.googleusercontent.com/d/" + item.image_url.replaceAll('https://drive.google.com/file/d/', '').replaceAll('/view?usp=sharing', '') + "?authuser=0"}
+                            src={"https://lh3.googleusercontent.com/d/" + filteredItem.image_url.replaceAll('https://drive.google.com/file/d/', '').replaceAll('/view?usp=sharing', '') + "?authuser=0"}
                             alt="technique"
                         />
                     </div>
