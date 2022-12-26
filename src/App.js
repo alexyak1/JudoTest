1a8e385c013d151bab3bd4e02720a1548d6b2a1a
Aleksandr Iakimchuk <yakimchuk.nstu@gmail.com>
2022-12-26 22:21:30 +0100

    remove youtube feature

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Navbar from "./components/NavigationComponents";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './pages';
 import Quiz from './pages/quiz';
 import Techniques from './pages/techniques';
 
@@ -13,7 +12,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Quiz />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/techniques" element={<Techniques />} />
       </Routes>
