954d4ad31b72cc8960dde5fd09ccac97163c2299
Alexander Yakimchuk <aleksandr.yakimchuk@kvdbil.se>
2025-04-10 12:38:41 +0200

    use http

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import Navbar from "./components/NavigationComponents";
 import Footer from "./components/NavigationComponents/footer";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Quiz from './pages/quiz';
 import Techniques from './pages/techniques';
 import Kata from "./pages/kata";
 
 function App() {
+  useEffect(() => {
+    // Redirect HTTPS to HTTP if the protocol is HTTPS
+    if (window.location.protocol === "https:") {
+      window.location.href = "http://" + window.location.hostname + window.location.pathname + window.location.search;
+    }
+  }, []);
+
   return (
     <Router>
       <Navbar />
@@ -18,7 +24,7 @@ function App() {
         <Route path="/techniques" element={<Techniques />} />
         <Route path="/kata" element={<Kata />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
