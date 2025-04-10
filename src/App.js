b5313f9182366e9190efd90b0a89bc559b6016c1
Alexander Yakimchuk <aleksandr.yakimchuk@kvdbil.se>
2025-04-10 12:55:19 +0200

    remove redirect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import './App.css';
 import Navbar from "./components/NavigationComponents";
 import Footer from "./components/NavigationComponents/footer";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Quiz from './pages/quiz';
 import Techniques from './pages/techniques';
 import Kata from "./pages/kata";
 
 function App() {
-  useEffect(() => {
-    if (window.location.protocol === "https:") {
-      window.location.replace("http://" + window.location.hostname + window.location.pathname + window.location.search);
-    }
-  }, []);
-
   return (
     <Router>
       <Navbar />
