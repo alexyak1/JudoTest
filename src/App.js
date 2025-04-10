03c143a69ae560292f4e0fb4c3279ecb17a84986
Alexander Yakimchuk <aleksandr.yakimchuk@kvdbil.se>
2025-04-10 14:45:23 +0200

    redirect to thhp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import Navbar from "./components/NavigationComponents";
 import Footer from "./components/NavigationComponents/footer";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Quiz from './pages/quiz';
 import Techniques from './pages/techniques';
 import Kata from "./pages/kata";
 
 function App() {
+  useEffect(() => {
+    // Redirect HTTPS to HTTP if the protocol is HTTPS
+    if (window.location.protocol === "https:") {
+      window.location.href = "http://" + window.location.hostname + window.location.pathname + window.location.search;
+    }
+  }, []);  // Empty dependency array ensures this runs only once, on component mount
+
   return (
     <Router>
       <Navbar />
